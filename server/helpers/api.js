7496904921e49ff116695fe30581fcbd57abb424
Mitch <mitch_goudkuil@hotmail.com>
2019-03-22 14:17:08 +0100

    added cache and images for questions

--- server/helpers/api.js
+++ server/helpers/api.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 async function getMovie() {
   // let data = await axios.get(`https://www.food2fork.com/api/search?key=8dd190f9de013f50b8709ccbd68ee782&q=${ingrArray.toString()}`)
   let data = await axios.get(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc?&api_key=c7cf7b22f11d0091d015ec5e5bbe8ca2`)
-  console.log(data.data.results);
+
   return data.data.results
 }
 
--- server/helpers/questions.js
+++ server/helpers/questions.js
@@ -1,17 +1,17 @@
 function getAll() {
   return [{
     id: 1,
-    img: "./img/brain.jpg",
+    img: "./img/",
     question: "Do you want to watch a movie or a serie?",
     tag: "First question",
-    answers: ['movie', 'serie']
+    answers: [{answer: 'movie', img:"../img/film.svg"}, {answer:'serie', img:"../img/television.svg"}]
   },
   {
     id: 2,
     img: "./img/genre.jpg",
     question: "What genre would you like to watch?",
     tag: "Second question",
-    answers: ['thriller', 'animation','horror', 'comedy']
+    answers: [{answer: 'thriller', img:"../img/film.svg"},{answer: 'animation', img:"../img/film.svg"},{answer:'horror', img:"../img/film.svg"},{answer: 'comedy', img:"../img/film.svg"}]
   }
 ]
 }
