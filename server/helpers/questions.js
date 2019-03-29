23deabf64e2bc03a1c72eea6bee3945c8b60d9ab
Mitch <mitch_goudkuil@hotmail.com>
2019-03-29 11:46:03 +0100

    added image paths

--- server/helpers/questions.js
+++ server/helpers/questions.js
@@ -4,14 +4,14 @@ function getAll() {
     img: "./img/",
     question: "Do you want to watch a movie or a serie?",
     tag: "First question",
-    answers: [{answer: 'movie', img:"../img/film.svg"}, {answer:'serie', img:"../img/television.svg"}]
+    answers: [{answer: 'movie', img:"./../server/assets/img/film.svg"}, {answer:'serie', img:"./../img/television.svg"}]
   },
   {
     id: 2,
     img: "./img/genre.jpg",
     question: "What genre would you like to watch?",
     tag: "Second question",
-    answers: [{answer: 'thriller', img:"../img/knife.svg"},{answer: 'drama', img:"../img/drama.svg"},{answer:'horror', img:"../img/skull.svg"},{answer: 'comedy', img:"../img/laughing.svg"}]
+    answers: [{answer: 'thriller', img:"./../img/knife.svg"},{answer: 'drama', img:"./../img/drama.svg"},{answer:'horror', img:"./../img/skull.svg"},{answer: 'comedy', img:"./../img/laughing.svg"}]
   }
 ]
 }
